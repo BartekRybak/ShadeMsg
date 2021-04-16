1b283a01c8021c9c7a52755c41d75419f514e0e3
Bartek Rybak <bartekrybakofficial@gmail.com>
2021-04-16 19:54:03 +0200

    packet security crypt/decrypt

---
+++ ShadeMsg/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using ShadeMsg.Network;
+using ShadeMsg.Security;
+
+namespace ShadeMsg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server s = new Server("asdf", 1234);
+
+            Console.ReadKey();
+        }
+    }
+}
